57eec800bfab8bfe6769e1b84b155d2160854a95
Thib-numberX <peronnothibault@hotmail.fr>
2022-04-02 11:51:21 +0200

    ajout du fichier data + images + wireframe

---
+++ data/characters.js
@@ -0,0 +1,80 @@
+export default [
+  {
+    name: 'Sonic',
+    description: 'Sonic est un hérisson supersonique le plus rapide du monde. Toujours en vadrouille, il va où le vent l\'emporte et vit sa vie selon ses propres règles plutôt que celles de ceux qui l\'entourent. Il est plutôt facile à vivre et aide toujours son prochain. Ce qu\'il déteste plus que tout c\'est Dr Eggman qui veut prendre le contrôle du monde.',
+    picture: '../pictures/sonic.jpg',
+    type: 'Gentil',
+  },
+  {
+    name: 'Amy Rose',
+    description: 'Amy Rose est une fille excessivement joyeuse et très amoureuse de Sonic qui veut se marier avec lui. Elle se sert de son Piko Piko Hammer pour se battre. Sonic la trouve plus ou moins embarrassante mais au fond de lui, il ne la déteste peut-être pas tant que ça.',
+    picture: '../pictures/amy.jpg',
+    type: 'Gentil',
+  },
+  {
+    name: 'Tails',
+    description: 'Miles « Tails » Prower surnommé Tails est un renard à deux queues et un véritable fou de la mécanique; ces compétences sont égales à celles du méchant Dr Egmann. Il fait toujours de son mieux pour aider Sonic, qui le considère plus ou moins comme son petit frère',
+    picture: '../pictures/tails.jpg',
+    type: 'Gentil',
+  },
+  {
+    name: 'Knuckles',
+    description: 'Knuckles est un échidné égoïste avec des dreadlocks de l\'île de l\'Ange. Il a été nommé gardien de la Master Emerauld. Il est dur, têtu et inflexible. Il se considère être le rival de Sonic, peut-être parce qu\'il rêve d\'un mode de vie plus libre. Il est également chasseur de trésors et compétent en arts martiaux.',
+    picture: '../pictures/knukles.jpg',
+    type: 'Gentil',
+  },
+  {
+    name: 'Cream',
+    description: 'Cream est une mignonne petite lapine qui emmène son minuscule Chao « Cheese » avec elle partout. Élevée comme une princesse, elle est un peu naïve et parfois simplette, et elle se laisse souvent embarquer dans les problèmes des autres. Elle aime aussi les glaces.',
+    picture: '../pictures/cream.jpg',
+    type: 'Gentil',
+  },
+  {
+    name: 'Big',
+    description: 'Big le chat est un énorme felin qui vit dans la paix et la tranquillité avec son meilleur ami Froggy, au milieu de la jungle. Son passe-temps préféré est la pêche. Il est fort mais doux et très relax. Il fait tout son possible pour aider ses amis.',
+    picture: '../pictures/big.jpg',
+    type: 'Gentil',
+  },
+  {
+    name: 'Shadow',
+    description: 'Shadow le hérisson noir est la sinistre incarnation de Sonic créée par Gérald Robotnik. Il ne vieillit jamais et peut utiliser une technique appelée « Chaos Control » pour fausser le temps et l\'espace.',
+    picture: '../pictures/shadow.jpg',
+    type: 'Méchant',
+  },
+  {
+    name: 'E-123 Omega',
+    description: 'E-123 Omega est le dernier et le plus puissant des robots de la série E du Dr Egmann. Omega a été réactivé accidentellement par Rouge alors qu\'elle libérait Shadow. A partir de ce moment-là, Omega a décidé de détruire toutes les machine du Dr Egmann pour se venger de lui.',
+    picture: '../pictures/e123.jpg',
+    type: 'Méchant',
+  },
+  {
+    name: 'Rouge',
+    description: 'Rouge est une chauve souris et une chasseuse de trésors professionnelle, toujours en quête de pierres les plus précieuses au monde. C\'est également une espionne du gouvernement à temps partiel. Intrépide, ravissante et débordante de charme féminin, elle se dévoue son travail qu\'elle fait jusqu\'au bout.',
+    picture: '../pictures/rouge.jpg',
+    type: 'Méchant',
+  },
+  {
+    name: 'Espio',
+    description: 'Espio est un caméléon dogmatique et imbu de sa personne qui travaille à l\'agence de détectives Chaotix. Il a une discipline militariste en dépit d\'être tranquille et désinvolte, et grâce à sa formation ninja, il peut faire face aux problèmes sans se soucier du danger.',
+    picture: '../pictures/espio.jpg',
+    type: 'Méchant',
+  },
+  {
+    name: 'Vector',
+    description: 'Vector le crocodile détective hors-la-loi et boss de l\'agence de détective Chaotix. Il peut trouver des solutions pacifiques aux problèmes qui se posent à lui, du moment qu\'il n\'est pas possible de trouver une méthode agressive.',
+    picture: '../pictures/vector.jpg',
+    type: 'Méchant',
+  },
+  {
+    name: 'Charmy',
+    description: 'Charmy l\'abeille est un gosse amusant et étourdi qui adore faire le pitre et donne l\'impression que le reste du personnel de Chaotix est professionnel. Il est généralement de bonne humeur et allègre mais lorsqu\'il se met en colère il a tendance à canaliser la source de cette colère dans sa queue piquante.',
+    picture: '../pictures/charmy.jpg',
+    type: 'Méchant',
+  },
+  {
+    name: 'Dr. Egmann',
+    description: "Dr. Egmann est un docteur qui a QI extraordinaire de 300. Egmann est un romantique, un féministe et il se veut être gentilhomme. Malheureusement, son charme est difficile à percevoir à travers l'abominable rire qui accompagne ses folles déclarations de suprématie mondiale. Sonic essaie de trouver un moyen de mettre fin à ses stratagèmes.",
+    picture: '../pictures/eggman.jpg',
+    type: 'Méchant',
+  },
+];
